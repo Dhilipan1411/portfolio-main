b215942ca262f2e61b41603a9aeccf429d8b7a74
Dhilipan <dhilipan1411@gmail.com>
2026-03-18 18:52:03 +0530

    route

--- src/Components/Router.jsx
+++ src/Components/Router.jsx
@@ -1,21 +1,21 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
 import Projects from "./Projects";
-import Header from "./Header";
-import Home from "./Home";
 
 function Router() {
   return (
-    <BrowserRouter>
-      <Header /> {/* 👈 always visible */}
+    <HashRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
